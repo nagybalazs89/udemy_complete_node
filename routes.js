5c6bedc1052ec18d24ffed2a7e02e0f6dba57d7e
Balazs Nagy/BDP/HU <balazs.nagy@avon.com>
2022-07-23 19:17:48 +0200

    Initial

---
+++ routes.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    if (req.url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<p>Hello from Slash23!</p>');
+        res.write('<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button>');
+        res.write('</html>');
+        return res.end();
+    }
+    
+    if (req.url === '/message' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<p>Hello from main!</p>');
+    res.write('</html>');
+    res.end();
+};
+
+module.exports = {
+    handler: requestHandler
+}
---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const rootDir = require('../util/path');
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res) => {
+    res.redirect('/');
+});
+
+module.exports = router;
